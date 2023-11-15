249c4566e1bba9aacf52aca6e18aa6287b2e1da3
Arif Hossan <arif.hossan.eu@gmail.com>
2023-11-15 22:28:10 +0600

    exploring express and dynamic data

---
+++ main.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const phones = require('./phones.json');
+const app = express();
+const port = 5000;
+
+app.get('/',(req,res)=>{
+    res.send('Phoone Data coming soon')
+})
+
+app.get('/phones',(req,res)=>{
+    res.send(phones);
+    console.log('phones are loaded');
+})
+
+// dynamically get the single phone
+app.get('/phones/:id',(req,res)=>{
+    const id = parseInt(req.params.id);
+    res.send(phones.find(phone => phone.id ===id));
+    console.log('i need data for phone', id);
+})
+
+app.listen(port,()=>{
+    console.log(`Phone server running on ${port}`);
+})
